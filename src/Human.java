997017058edcba72aa368f35a033fe1141f667f3
EgKataev <113854561+EgKataev@users.noreply.github.com>
2022-11-15 13:28:17 +0300

    Закончил третью часть домашки!!!

--- src/Human.java
+++ src/Human.java
@@ -5,9 +5,32 @@ public class Human {
     public String jobTitle;
 
     public Human(int age, String name, String city, String jobTitle) {
-        this.age = age;
-        this.name = name;
-        this.city = city;
-        this.jobTitle = jobTitle;
+        if (age < 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+
+
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+
+
+        }
+        if (city == null) {
+            this.city = "Информация не указана";
+        } else {
+            this.city = city;
+
+
+        }
+        if (jobTitle == null) {
+            this.jobTitle = "Информация не указана";
+        } else {
+            this.jobTitle = jobTitle;
+
+        }
     }
 }
