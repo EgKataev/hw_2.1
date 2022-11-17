56c7bd3aaf753a2ec5db1c905a1b7563f0f790fd
EgKataev <113854561+EgKataev@users.noreply.github.com>
2022-11-17 22:31:02 +0300

    Инкапсуляция, домашка, первая часть, начало!!!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задания 1");
+        System.out.println("Задания №1");
 
         int currentYear = 2022;
 
