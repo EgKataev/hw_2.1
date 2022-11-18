4250fc0b9dc8943e49408ce496cc52402c96fc0a
EgKataev <113854561+EgKataev@users.noreply.github.com>
2022-11-18 09:35:02 +0300

    Инкапсуляция, домашка, первая часть, закончил!!! Что-то не то написал!!! Проверьте, пжл))

--- src/Human.java
+++ src/Human.java
@@ -1,36 +1,57 @@
 public class Human {
-    public int age;
+    private int yearOfBirth;
     public String name;
-    public String city;
+    private String city;
     public String jobTitle;
 
-    public Human(int age, String name, String city, String jobTitle) {
-        if (age < 0) {
-            this.age = 0;
-        } else {
-            this.age = age;
-        }
+    public Human(int yearOfBirth, String name, String city, String jobTitle) {
+//        if (yearOfBirth < 0) {
+//            this.yearOfBirth = 0;
+//        } else {
+//            this.yearOfBirth = yearOfBirth;
+//        }
 
 
         if (name == null) {
             this.name = "Информация не указана";
         } else {
             this.name = name;
-
-
         }
-        if (city == null) {
-            this.city = "Информация не указана";
-        } else {
-            this.city = city;
 
+//        if (city == null) {
+//            this.city = "Информация не указана";
+//        } else {
+//            this.city = city;
+//        }
 
-        }
         if (jobTitle == null) {
             this.jobTitle = "Информация не указана";
         } else {
             this.jobTitle = jobTitle;
+        }
+    }
 
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        if (city == null || city.isBlank() || city.isEmpty()) {
+            this.city = "Информация не указана";
+        } else {
+            this.city = city;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задания №1");
+        System.out.println("Задания 1");
 
         int currentYear = 2022;
 
-        Human maksim = new Human(35, "Максим", "Минск", "бренд-менеджер");
+        Human maksim = new Human(currentYear - 35, "Максим", "Минск", "бренд-менеджер");
 
-        Human anya = new Human(29, "Аня", "Москва", "методист образовательных программ");
+        Human anya = new Human(currentYear - 29, "Аня", "Москва", "методист образовательных программ");
 
-        Human katya = new Human(28, "Катя", "Калининград", "продакт-менеджер");
+        Human katya = new Human(currentYear - 28, "Катя", "Калининград", "продакт-менеджер");
 
-        Human artem = new Human(27, "Артём", "Москва", "директор по развитию бизнеса");
+        Human artem = new Human(currentYear - 27, "Артём", "Москва", "директор по развитию бизнеса");
 
-        System.out.println("Привет! Меня зовут " + maksim.name + ". Я из города " + maksim.city + ". Я родился в " + (currentYear - maksim.age) + " году. Я работаю на должности: " + maksim.jobTitle + ". Будем знакомы!");
-        System.out.println("Привет! Меня зовут " + anya.name + ". Я из города " + anya.city + ". Я родился в " + (currentYear - anya.age) + " году. Я работаю на должности: " + anya.jobTitle + ". Будем знакомы!");
-        System.out.println("Привет! Меня зовут " + katya.name + ". Я из города " + katya.city + ". Я родился в " + (currentYear - katya.age) + " году. Я работаю на должности: " + katya.jobTitle + ". Будем знакомы!");
-        System.out.println("Привет! Меня зовут " + artem.name + ". Я из города " + artem.city + ". Я родился в " + (currentYear - artem.age) + " году. Я работаю на должности: " + artem.jobTitle + ". Будем знакомы!");
+        Human vladimir = new Human(currentYear - 21, "Владимир", "Казань", null);
+
+        printInfo(maksim);
+        printInfo(anya);
+        printInfo(katya);
+        printInfo(artem);
+        printInfo(vladimir);
+
+
         System.out.println();
 
         System.out.println("Задание 2");
@@ -90,5 +95,9 @@ public class Main {
                 ", цвет кузова- " + hyundai.color +
                 ", год производства- " + hyundai.productionYear +
                 ", страна производства- " + hyundai.productionCountry);
+    }
+
+    private static void printInfo(Human human) {
+        System.out.println("Привет! Меня зовут " + human.name + ". Я из города " + human.getCity() + ". Я родился в " + human.getYearOfBirth() + " году. Я работаю на должности: " + human.jobTitle + ". Будем знакомы!");
     }
 }
